4f9f1e1d5829ce43e32fe6e8d91c8c44206c2c32
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2016-03-18 09:14:13 +0200

    Initial commit.

---
+++ server.js
@@ -0,0 +1,54 @@
+var Hapi = require('hapi');
+var Joi = require('joi');
+var superagent = require('superagent');
+var server = new Hapi.Server();
+
+// Server config.
+server.connection({ port: process.env.PORT || 8080 });
+
+// Helpers for timeout.
+var TIMEOUT_DURATION = 1000 * 60 * 5; // 5mins
+var timeout = null;
+
+// Routing.
+server.route({
+  method: 'POST',
+  path: '/',
+  config: {
+    validate: {
+      // Validate body.
+      payload: {
+        url: Joi.string().uri({ scheme: ['https'] }).required(),
+        event: Joi.string().valid('message').required(),
+        content: Joi.string().required(),
+        external_user_name: Joi.string().required()
+      }
+    }
+  },
+  handler: function(request, reply) {
+    // Allow only one timeout set. Poor mans throttling ;(
+    if (timeout) {
+      return reply('ignored');
+    }
+
+    // Set timeout.
+    timeout = setTimeout(function() {
+      // Send request to given url.
+      superagent.post(request.payload.url)
+      .send({
+        event: request.payload.event,
+        content: request.payload.content,
+        external_user_name: request.payload.external_user_name
+      });
+    }, TIMEOUT_DURATION);
+
+    return reply('success');
+  }
+});
+
+// Start server.
+server.start(function(error) {
+  if (error) {
+    throw error;
+  }
+});
